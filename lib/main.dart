31952911d9691219526b0a2744deda2c451ca6e5
Roshan Gurung <roshc10grg@gmail.com>
2024-06-30 23:17:16 +0545

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       floatingActionButton: Container(
           decoration: BoxDecoration(
-              color: Colors.red.shade200,
-              borderRadius: BorderRadius.circular(30)),
+              color: Colors.black54, borderRadius: BorderRadius.circular(50)),
           child: IconButton(
               onPressed: null,
               icon: Icon(
@@ -41,82 +40,109 @@ class _HomePageState extends State<HomePage> {
               ))),
       appBar: AppBar(
         backgroundColor: Colors.blueGrey.shade200,
-        leading: null,
-        flexibleSpace: Container(
-          width: 100,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            // ignore: prefer_const_constructors
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 10,
-                ),
-                Container(
-                    decoration: BoxDecoration(
-                        color: Colors.red.shade100,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: ElevatedButton(
-                        onPressed: null,
-                        child: Text(
-                          "ALL",
-                          style: TextStyle(color: Colors.white),
-                        ))),
-                SizedBox(
-                  width: 30,
-                  height: 100,
-                ),
-                Container(
-                    decoration: BoxDecoration(
-                        color: Colors.red.shade100,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: ElevatedButton(
-                        onPressed: null,
-                        child: Text(
-                          "Work",
-                          style: TextStyle(color: Colors.white),
-                        ))),
-                SizedBox(
-                  width: 30,
-                  height: 100,
-                ),
-                Container(
-                    decoration: BoxDecoration(
-                        color: Colors.red.shade100,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: ElevatedButton(
-                        onPressed: null,
-                        child: Text(
-                          "Personal",
-                          style: TextStyle(color: Colors.white),
-                        ))),
-                SizedBox(
-                  width: 30,
-                  height: 100,
-                ),
-              ],
-            ),
-          ),
+        leading: DrawerButton(
+          color: Colors.white,
         ),
         actions: [
-          IconButton(
-              onPressed: null,
-              icon: Icon(
-                Icons.more_vert_outlined,
-                color: Colors.white,
-              ))
+          GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return Dialog(
+                    child: InteractiveViewer(
+                      boundaryMargin: EdgeInsets.all(20.0),
+                      minScale: 0.1,
+                      maxScale: 2.0,
+                      child: Image.asset('assets/images/roshan.jpg'),
+                    ),
+                  );
+                },
+              );
+            },
+            child: CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage('assets/images/roshan.jpg'),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.blueGrey.shade300,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Container(
+              width: 350,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  prefixIcon: IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                        size: 25,
+                      )),
+                  hintText: "search",
+                  hintStyle: TextStyle(color: Colors.black, fontSize: 22),
+                  suffixIcon: Icon(
+                    Icons.mic,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 230),
+            child: Text(
+              "ALL ToDos",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+
+          Expanded(
+            child: Center(
+              child: Text(
+                "No Task in list \nClick '+' to add a new task.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 23,),
+              ),
+            ),
+          )
         ],
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.blueGrey.shade300,
+        color: Colors.blueGrey.shade200,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            IconButton(onPressed: null, icon: Icon(Icons.menu,color: Colors.white,size: 35,)),
-            IconButton(onPressed: null, icon: Icon(Icons.task_rounded,color: Colors.white,size: 35,)),
-            IconButton(onPressed: null, icon: Icon(Icons.person_2,color: Colors.white,size: 35,)),
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.task_rounded,
+                  color: Colors.white,
+                  size: 35,
+                )),
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.person_2,
+                  color: Colors.white,
+                  size: 35,
+                )),
           ],
         ),
       ),
